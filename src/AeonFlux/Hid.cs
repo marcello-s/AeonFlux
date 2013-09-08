78e876aaea4fac002e906830802e91b3be3b8d7b
marcellos <marcello.s@gmx.ch>
2013-09-08 22:13:24 +0200

    AeonFlux - communication library, tests assembly

---
+++ src/AeonFlux/Hid.cs
@@ -0,0 +1,55 @@
+﻿#region license and copyright
+/*
+ * The MIT License, Copyright (c) 2013 Marcel Schneider
+ * for details see License.txt
+ */
+#endregion
+        
+namespace AeonFlux
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal static class Hid
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct HidCaps
+        {
+            public short Usage;
+            public short UsagePage;
+            public short InputReportByteLength;
+            public short OutputReportByteLength;
+            public short FeatureReportByteLength;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 17)]
+            public short[] Reserved;
+            public short NumberLinkCollectionNodes;
+            public short NumberInputButtonCaps;
+            public short NumberInputValueCaps;
+            public short NumberInputDataIndices;
+            public short NumberOutputButtonCaps;
+            public short NumberOutputValueCaps;
+            public short NumberOutputDataIndices;
+            public short NumberFeatureButtonCaps;
+            public short NumberFeatureValueCaps;
+            public short NumberFeatureDataIndices;
+        }
+
+        [DllImport("hid.dll", SetLastError = true)]
+        internal static extern void HidD_GetHidGuid(
+            out Guid hidGuid);
+
+        [DllImport("hid.dll", SetLastError = true)]
+        internal static extern bool HidD_GetPreparsedData(
+            IntPtr handle,
+            out IntPtr data);
+
+        [DllImport("hid.dll", SetLastError = true)]
+        internal static extern bool HidD_FreePreparsedData(
+            ref IntPtr data);
+
+        [DllImport("hid.dll", SetLastError = true)]
+        internal static extern int HidP_GetCaps(
+            IntPtr data,
+            out HidCaps caps);
+    }
+}
---
+++ src/AeonFlux/UsbDevice.cs
@@ -0,0 +1,153 @@
+﻿#region license and copyright
+/*
+ * The MIT License, Copyright (c) 2013 Marcel Schneider
+ * for details see License.txt
+ */
+#endregion
+        
+namespace AeonFlux
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using Microsoft.Win32.SafeHandles;
+
+    internal abstract class UsbDevice : IDisposable
+    {
+        protected SafeFileHandle _handle;
+        protected int _inputBufferLength;
+        protected int _outputBufferLength;
+        protected FileStream _fs;
+
+        public Action<IOException> DeviceRemovedHandler = ex => { };
+        protected Action<byte[]> DataReceivedHandler = buffer => { };
+
+        ~UsbDevice()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            if (_fs != null)
+            {
+                _fs.Close();
+                _fs = null;
+            }
+            if (_handle != null && !_handle.IsClosed)
+                Kernel32.CloseHandle(_handle);
+        }
+
+        private static string GetDevicePath(IntPtr infoSet, ref SetupApi.DeviceInterfaceData interfaceData)
+        {
+            uint requiredSize = 0;
+            if (!SetupApi.SetupDiGetDeviceInterfaceDetail(
+                infoSet, 
+                ref interfaceData, 
+                IntPtr.Zero, 
+                0, 
+                ref requiredSize, 
+                IntPtr.Zero))
+            {
+                var detailData = new SetupApi.DeviceInterfaceDetailData();
+                detailData.Size = Environment.Is64BitProcess ? 8 : 5;
+                if (SetupApi.SetupDiGetDeviceInterfaceDetail(
+                    infoSet,
+                    ref interfaceData,
+                    ref detailData,
+                    requiredSize,
+                    ref requiredSize,
+                    IntPtr.Zero))
+                {
+                    return detailData.DevicePath;                    
+                }
+            }
+            return null;
+        }
+
+        public static IEnumerable<string> Enumerate(Guid classGuid)
+        {
+            var devicePathList = new List<string>();
+            var infoSet = SetupApi.SetupDiGetClassDevs(
+                ref classGuid, 
+                null, 
+                IntPtr.Zero, 
+                SetupApi.DIGCF_PRESENT | SetupApi.DIGCF_DEVICEINTERFACE);
+
+            var interfaceData = new SetupApi.DeviceInterfaceData();
+            interfaceData.Size = Marshal.SizeOf(interfaceData);
+
+            try
+            {
+                var index = 0;
+                while (SetupApi.SetupDiEnumDeviceInterfaces(infoSet, 0, ref classGuid, (uint)index, ref interfaceData))
+                {
+                    devicePathList.Add(GetDevicePath(infoSet, ref interfaceData));
+                    index++;
+                }
+            }
+            finally
+            {
+                SetupApi.SetupDiDestroyDeviceInfoList(infoSet);
+            }
+            return devicePathList;
+        }
+
+        public static object CreateDevice(Guid classGuid, int vid, int pid, Type deviceType)
+        {
+            var vendorProduct = string.Format("vid_{0:x4}&pid_{1:x4}", vid, pid);
+            var devicePath = Enumerate(classGuid).FirstOrDefault(d => d.IndexOf(vendorProduct) >= 0);
+            if (!string.IsNullOrEmpty(devicePath))
+            {
+                var usbDevice = (UsbDevice)Activator.CreateInstance(deviceType);
+                usbDevice.Initialize(devicePath);
+                return usbDevice;
+            }
+            return null;
+        }
+
+        protected abstract void Initialize(string devicePath);
+
+        protected virtual void BeginAsyncRead()
+        {
+            var buffer = new byte[_inputBufferLength];
+            _fs.BeginRead(buffer, 0, _inputBufferLength, ReadCompleted, buffer);
+        }
+
+        protected virtual void ReadCompleted(IAsyncResult result)
+        {
+            var buffer = result.AsyncState as byte[];
+            try
+            {
+                _fs.EndRead(result);
+                try
+                {
+                    DataReceivedHandler(buffer);
+                }
+                finally
+                {
+                    BeginAsyncRead();
+                }
+            }
+            catch (IOException ex)
+            {
+                DeviceRemovedHandler(ex);
+                Dispose();
+            }
+        }
+
+        protected virtual void Write(byte[] buffer)
+        {
+            _fs.Write(buffer, 0, _outputBufferLength);
+        }
+    }
+}
