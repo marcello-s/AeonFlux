9488b869edb29a367dc9ffad577ad418297c6f08
marcellos <marcello.s@gmx.ch>
2015-07-05 12:30:14 +0200

    AeonFlux - add xunit console runner to csharp build script

--- src/csharp/default.ps1
+++ src/csharp/default.ps1
@@ -1,41 +1,54 @@
 properties {
 # *** CONFIGURE ***
-  $baseFolder = Resolve-Path .
-  $outFolder = "$baseFolder\bin"
+  $baseFolder = Resolve-Path -Relative .
+  $outFolder = "$baseFolder\..\..\bin"
   $configuration = "Debug"
   $platform = "Any CPU"
   $target = "Build"
-  $solution = "$baseFolder\src\csharp\AeonFlux\AeonFlux.sln"
+  $solution = "$baseFolder\AeonFlux\AeonFlux.sln"
   $contracts = "CONTRACTS_FULL"
 
 # *** ENVIRONMENT ***
   $msbuildPath = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-  # $nunit = Get-ChildItem . -Filter "nunit-console.exe" -Recurse | foreach{$_.FullName}
-  # $nunitTestFrameworkVersion = "4.0.30319"
+  $xunitRunner = Get-ChildItem . -Filter "xunit.console.exe" -Recurse | foreach {$_.FullName}
 }
 
-task Default -depends Test
 
-task Test -depends CompileSolution, Clean { 
-  # run tests
-  # $testBinaries = Get-ChildItem $outFolder\*Tests.dll
-  # $testAssemblies = ""
-  # $testBinaries | Foreach-Object {$testAssemblies +=  """.\bin\" + $_.Name + """ "}
-  # &$nunit /framework=$nunitTestFrameworkVersion $testAssemblies.Trim()
+# *** functions for clean, compile, test-runner, package, etc. ***
+function RemoveFolder($folder) {
+  Assert($folder -ne $null) "'folder' must not be null"
+  Remove-Item -force $folder -Recurse -ErrorAction SilentlyContinue | Out-Null
 }
 
-function Compile($solution) {
-  Assert($solution -ne $null) "'solution' should not be null" 
-  
-  # if build utilities v3.5 is loaded, the system cannot load any v4.0 assemblies :(
+function CreateFolder($folder) {
+  Assert($folder -ne $null) "'folder' must not be null"
+  New-Item -ItemType Directory -Force -Path "$folder" | Out-Null
+}
+
+function Compile($solution, $target) {
+  Assert($solution -ne $null) "'solution' must not be null" 
+  Assert($target -ne $null) "'' must not be null"
   $msbuild = $msbuildPath
-  &$msbuild /target:"$target" /p:Configuration="$configuration" /p:Platform="$platform" /p:OutDir="$outFolder"\\ "$solution" /m
+  &$msbuild /target:"$target" /p:Configuration="$configuration" /p:Platform="$platform" "$solution" /m /noconlog /fl "/flp:errorsonly;logfile=""$outFolder\errors.log"""
 }
+
+
+
 
 task CompileSolution -depends Clean { 
-  Compile($solution)
+  Compile $solution $target
 }
 
 task Clean { 
-  Remove-Item -force $outFolder -Recurse -ErrorAction SilentlyContinue | Out-Null
+  RemoveFolder($outFolder)
+  CreateFolder($outFolder)
 }
+
+task Test -depends CompileSolution, Clean { 
+  $testBinaries = Get-ChildItem "$outFolder\csharp\*Tests.dll"
+  $testAssemblies = ""
+  $testBinaries | Foreach-Object {$testAssemblies += """..\..\bin\csharp\" + $_.Name + """ "} 
+  &$xunitRunner $testAssemblies.Trim() -nologo -quiet -xml "$outFolder\xunit.xml"
+}
+
+task Default -depends Test
